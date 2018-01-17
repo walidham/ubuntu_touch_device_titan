6755d581d8d424731763c19965266630211c46e8
Walid HAMMAMI <walid.ham@hotmail.com>
2018-01-17 22:23:45 +0100

    channge kernel path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_SOURCE := kernel/motorola/kernel_thea_msm8226
+TARGET_KERNEL_SOURCE := kernel/motorola/msm8226
 
 
 # Vendor Unification Init
